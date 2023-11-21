ca44e9775770b255c91d931726ebaa4bcc7e5741
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-21 06:21:58 +0000

    data mapped to the frontend

--- frontend/src/pages/Items.jsx
+++ frontend/src/pages/Items.jsx
@@ -9,7 +9,7 @@ export const Items = () => {
             try{
                 const res = await axios.get('http://localhost:8800/items');
                 console.log(res)
-                // setItems()
+                setItems(res.data)
             }catch(err){
                 console.log(err)
             }
@@ -18,7 +18,19 @@ export const Items = () => {
     },[])
     return(
         <div>
-            items
+            <h1>My little Supermarket</h1>
+            <div className="items">
+                {items.map(item =>(
+                    <div className="item" key={item.id}>
+                        <img src={item.pic} alt=""/>
+                        <h2>{item.name}</h2>
+                        <span>{item.barcode}</span>
+                        <span>{item.price}</span>
+                        {item.specialPrice && <span>{item.specialPrice}</span>}
+                        <p>{item.about}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
