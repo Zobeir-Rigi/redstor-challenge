823c1771dde1c889b1180512778a855d9df3b0b5
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-21 03:10:07 +0000

    pg installed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express()
 
+// app.get("/", (req, res)=>{
+//     res.json("hello this is the backend")
+// })
+
 app.listen(8800, ()=>{
     console.log("connected to backend!")
 })
