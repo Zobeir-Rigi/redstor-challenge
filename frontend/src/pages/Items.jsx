b93c669eccd2287abf4fa7d254f3e6331c80faa0
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-21 05:40:52 +0000

    i can use axios but i prefer fetch method

--- frontend/src/pages/Items.jsx
+++ frontend/src/pages/Items.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
+
 export const Items = () => {
+    const [items, setItems] = useState([])
+
+    useEffect(()=>{
+        const fetchAllItems = async () => {
+            try{
+                const res = await fetch('http://localhost:8800/items');
+                console.log(res)
+                // setItems()
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllItems()
+    },[])
     return(
         <div>
-            Items
+            items
         </div>
     )
 }
