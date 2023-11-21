0219068b3da3b9c02775c29373dfe6b790e818b1
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-21 05:06:50 +0000

    react-router-dom created and also two page added to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css"
+import {BrowserRouter, Routes, Route,} from "react-router-dom"
+import { Items } from "./pages/Items";
+import { Checkout } from "./pages/Checkout";
 export const App = () => {
   return (
     <div className="App">
-      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Items />}/>
+        <Route path="/checkout" element = {<Checkout />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Items.jsx
@@ -0,0 +1,7 @@
+export const Items = () => {
+    return(
+        <div>
+            Items
+        </div>
+    )
+}
