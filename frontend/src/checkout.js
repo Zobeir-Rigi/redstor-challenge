981af3baf7ad180f460e62600d3be1c1e8ad8e09
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-29 14:56:30 +0000

    checkout.js added

---
+++ frontend/src/checkout.js
@@ -0,0 +1,59 @@
+
+class Checkout {
+    constructor(pricingRules) {
+      this.pricingRules = pricingRules;
+      this.basket = [];
+    }
+  
+    get ViewBasket() {
+      return this.basket;
+    }
+  
+    Scan(item) {
+      this.basket.push(item);
+    }
+  
+    get Total() {
+      let total = 0;
+  
+      const itemMap = {};
+  
+      this.basket.forEach((item) => {
+        itemMap[item] ? (itemMap[item] += 1) : (itemMap[item] = 1);
+      });
+  
+      Object.keys(itemMap).forEach((item) => {
+  
+        const numberOfItems = itemMap[item];
+  
+        const unitPrice = this.pricingRules[item].unitPrice;
+  
+        if (this.pricingRules[item].specialPriceString) {
+  
+          const specialPriceStringArray =
+            this.pricingRules[item].specialPriceString.split(" ");
+  
+          const specialPriceQuantity = Number(specialPriceStringArray[0]);
+  
+          const totalSpecialPrice = Number(specialPriceStringArray[2]);
+  
+          const numberOfSpecialPriceDeals = Math.floor(
+            numberOfItems / specialPriceQuantity
+          );
+  
+          total += numberOfSpecialPriceDeals * totalSpecialPrice;
+  
+          const remainingNormalPriceItems = numberOfItems % specialPriceQuantity;
+  
+          total += remainingNormalPriceItems * unitPrice;
+        } else {
+          
+          total += numberOfItems * unitPrice;
+        }
+      });
+  
+      return total;
+    }
+  }
+  
+  module.exports = Checkout;
